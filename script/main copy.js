614de15a7783203ea6078c22b49578b6604720ed
Owens94819 <>
2011-07-11 01:56:51 +0100

    updated

--- script/main copy.js
+++ script/main copy.js
@@ -1,12 +1,13 @@
 "use strict";
-const canvas_wrapper = document.querySelector('.canvas_wrapper foreignObject'),
+const canvas_wrapper = document.querySelector('.canvas_wrapper'),
+ canvas_board = document.querySelector('.canvas_board'),
     canvas_state = [],
     transform_speed = 2,
     canvas_reshape = {
         right: function (event, elm) {
             var p = elm.parentElement.querySelector('canvas');
             // var pX = 0
-            canvas_wrapper.onmousemove = function (e) {
+            canvas_board.onmousemove = function (e) {
                 p.style.height = p.offsetHeight + "px"
                 // if (pX > e.clientX) {
                 //     p.style.width = (p.offsetWidth - (-(e.movementX||e.webkitMovementX||-1))) + "px";
@@ -22,7 +23,7 @@ const canvas_wrapper = document.querySelector('.canvas_wrapper foreignObject'),
         bottom: function (event, elm) {
             var p = elm.parentElement.querySelector('canvas');
             // var pY = 0
-            canvas_wrapper.onmousemove = function (e) {
+            canvas_board.onmousemove = function (e) {
                 p.style.width = p.offsetWidth + "px";
                 p.style.height = (p.offsetHeight + ((e.movementY || e.webkitMovementY || -1))) + "px";
 
@@ -39,7 +40,7 @@ const canvas_wrapper = document.querySelector('.canvas_wrapper foreignObject'),
         "bottom-r": function (event, elm) {
             var p = elm.parentElement.querySelector('canvas');
             var pY = 0
-            canvas_wrapper.onmousemove = function (e) {
+            canvas_board.onmousemove = function (e) {
                 if (pY > e.clientY) {
                     p.style.height = (p.offsetHeight - transform_speed) + "px"
                     p.style.width = (p.offsetWidth - transform_speed) + "px";
@@ -79,7 +80,7 @@ const canvas_wrapper = document.querySelector('.canvas_wrapper foreignObject'),
             // e.style.pointerEvents ="none"
             // canvas_wrapper.setAttribute('mode', 'edit')
             // console.log(event);
-            canvas_wrapper.onmousemove = function (ev) {
+            canvas_board.onmousemove = function (ev) {
                 // if (ev.offsetX === 0 || ev.offsetY === 0) {
                 //     return
                 // }
@@ -128,23 +129,23 @@ const canvas_wrapper = document.querySelector('.canvas_wrapper foreignObject'),
     };
 
 let canvas = document.createElement('canvas')
-let canvas_div = document.createElement('xhtml:div')
+let canvas_div = document.createElement('div')
 canvas_div.innerHTML = `
-<xhtml:div d="center"></xhtml:div>
+<div d="center"></div>
 
-<xhtml:div d="bottom"></xhtml:div>
-<xhtml:div d="right"></xhtml:div>
+<div d="bottom"></div>
+<div d="right"></div>
 
 
-<xhtml:div d="bottom-r"></xhtml:div>
+<div d="bottom-r"></div>
 `
-canvas_div.getElementsByTagName('xhtml:div').forEach(function (e) {
+canvas_div.querySelectorAll('div').forEach(function (e) {
     if (canvas_reshape.hasOwnProperty(e.getAttribute('d'))) {
         e.setAttribute('onmousedown', `canvas_reshape['${e.getAttribute('d')}'](event,this);
         var elm=this
-        canvas_wrapper.onmouseleave=canvas_wrapper.onmouseup=function(){
+        canvas_board.onmouseleave=canvas_board.onmouseup=function(){
             // console.log('up');
-            canvas_wrapper.onmouseleave=canvas_wrapper.onmouseup=elm.onmouseleave=canvas_wrapper.onmousemove=null
+            canvas_board.onmouseleave=canvas_board.onmouseup=elm.onmouseleave=canvas_board.onmousemove=null
             // console.log(elm);
             // elm.parentElement.style.pointerEvents =""
             // canvas_wrapper.removeAttribute('mode')
@@ -154,38 +155,177 @@ canvas_div.getElementsByTagName('xhtml:div').forEach(function (e) {
     }
 })
 
-function getImageBlog() {
-    var img = new Image();
-    img.src = "image/1.png";
+console.log(canvas.getContext('2d'));
 
+
+canvas_wrapper._append = function (e, stored) {
+    if (!e._id) {
+        e._id = id()
+    }
+    // e.setAttribute("active","")
+
+    e.onclick = function () {
+
+        if (e.hasAttribute('active')) {
+            return
+        }
+        var pa = canvas_wrapper.querySelector('div[active]');
+        if (pa) {
+            pa.onmouseup = pa.onmousedown = null
+            pa.removeAttribute('active')
+            pa = void 0
+        }
+
+        e.setAttribute("active", "")
+
+        e.onmouseup = function () {
+            canvas_board.onmousemove = null
+        }
+
+        canvas_board.onclick=function(e){
+            if (e.target===this) {
+        var pa = canvas_wrapper.querySelector('div[active]');
+                if (pa) {
+                    pa.onmouseup = pa.onmousedown = null
+                    pa.removeAttribute('active')
+                    pa = void 0
+                }
+                canvas_board.onclick=null
+            }
+        }
+    }
+
+    this.appendChild(e)
+    if (!stored) {
+        var canv = e.querySelector('canvas')
+        var _name = id();
+        store.setItem(e._id, {
+            original_height: canv.height,
+            original_width: canv.width,
+            height: canv.offsetHeight,
+            width: canv.offsetWidth,
+            x: e.offsetLeft,
+            y: e.offsetTop,
+            // data:e.getContext('2d').getImageData(0,0,e.height,e.height)
+            data: _name
+        }).then(function () {
+            storeIMGD.setItem(_name, canv.getContext('2d').getImageData(0, 0, canv.height, canv.height))
+        });
+    }
+}
+
+function insert(data) {
+    canvas = canvas.cloneNode();
+    canvas.width = data.width
+    canvas.height = data.height
+    canvas.getContext('2d').putImageData(data, 0, 0)
+    canvas_wrapper.appendChild(canvas)
+}
+
+function load(id, data) {
+    storeIMGD.getItem(data.data).then(function () {
+        canvas = canvas.cloneNode();
+        canvas.width = data.original_width
+        canvas.height = data.original_height
+
+        canvas.getContext('2d').putImageData(arguments[0], 0, 0)
+
+        var _canvas_div = canvas_div.cloneNode(true)
+        _canvas_div._id = id
+        _canvas_div.appendChild(canvas)
+
+        _canvas_div.style.left = `${data.x}px`
+        _canvas_div.style.top = `${data.y}px`
+        canvas_wrapper._append(_canvas_div, true)
+    });
+
+}
+
+function getImage(src, stored) {
+    var img = new Image();
+    img.src = "image/1.png"
     img.onload = function () {
-        // img.style.borderRadius="600px"
+        img.style.borderRadius="600px"
         canvas = canvas.cloneNode();
         canvas.width = img.width / 2
         canvas.height = img.height / 2
 
         canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height)
 
-        canvas.toBlob(function(e){
-            console.log(e);
-        },"image/webp",9)
-        // var _canvas_div = canvas_div.cloneNode(true)
-        console.log(canvas);
+        var _canvas_div = canvas_div.cloneNode(true)
+        _canvas_div.appendChild(canvas)
+
+        _canvas_div.style.left = `${(canvas_wrapper.offsetWidth-canvas.width)/2}px`
+        _canvas_div.style.top = `${(canvas_wrapper.offsetHeight-canvas.height)/2}px`
+
+
+        canvas_wrapper._append(_canvas_div,stored)
     }
 }
 
+function loadData(id, data) {
+    storeIMGD.getItem(data.data).then(function (e) {
+        canvas = canvas.cloneNode();
+        canvas.width = e.width;
+        canvas.height = e.height;
 
-getImageBlog()
-function getImage() {
-    var img = new Image();
-    img.src = "image/1.png";
+        canvas.style.width=`${data.width}px`
+        canvas.style.height=`${data.height}px`
+
+        // var _canvas = canvas.cloneNode();
+        // _canvas.width = arguments[0].width;
+        // _canvas.height = arguments[0].height;
+        // _canvas.getContext('2d').putImageData(arguments[0], 0, 0)
+        // canvas.getContext('2d').drawImage(_canvas, 0, 0, canvas.width, canvas.height)
 
 
-    console.log();
+        canvas.getContext('2d').putImageData(arguments[0], 0, 0)
+
+        var _canvas_div = canvas_div.cloneNode(true)
+        _canvas_div._id=id
+        _canvas_div.appendChild(canvas)
+
+        _canvas_div.style.left = `${data.x}px`
+        _canvas_div.style.top = `${data.y}px`
+        canvas_wrapper._append(_canvas_div, true)
+
+    });
+
 }
-// console.log(canvas.getContext('2d'));
-function load() {
-    
+getImage.load = function (img) {
+    canvas = canvas.cloneNode();
+    canvas.width = img.width / 2
+    canvas.height = img.height / 2
+
+    canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height)
+
+    var _canvas_div = canvas_div.cloneNode(true)
+    _canvas_div.appendChild(canvas)
+
+    _canvas_div.style.left = `${(canvas_wrapper.offsetWidth-_canvas_div.width)/2}px`
+    _canvas_div.style.top = `${(canvas_wrapper.offsetHeight-_canvas_div.height)/2}px`
+
+    canvas_wrapper._append(_canvas_div, stored)
+}
+
+
+function update(elm, type) {
+    // console.log(elm._id);
+    store.getItem(elm._id).then(function (e) {
+        if (!e) {
+            console.error('something unusual!!!')
+            return
+        }
+        if (type === 'center') {
+            e.x = elm.offsetLeft
+            e.y = elm.offsetTop
+        } else {
+            var c = elm.querySelector('canvas')
+            e.width = c.offsetWidth;
+            e.height = c.offsetHeight;
+        }
+        store.setItem(elm._id, e)
+    });
 }
 
 function id() {
@@ -202,8 +342,8 @@ ready.then(function (e) {
     storeIMGD = e
     store.getAllItem(function (id, data) {
         if (data.original_height !== data.height || data.original_width !== data.width) {
-            return
             loadData(id, data)
+            return
         }
         load(id, data)
     })
