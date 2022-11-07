0ea735ba5f1a3ae69fb187b3a5c80aed75982831
Owens94819 <>
2022-11-07 07:37:30 +0100

    updated

--- script/db.js
+++ script/db.js
@@ -10,32 +10,51 @@ var db = {
 }
 
 var ready = new Promise(function (_r) {
-    db.db.forceOpen(project_name, 'image logs').then(function (e) {
+    db.db.forceOpen(app_id, project_name).then(function (e) {
+        if (e) {
+            db.project = e;
+        } else {
+            return db.db.create(app_id, project_name).then(function (e) {
+                db.project = e;
+            })
+        }
+    }).then(function(){
+    db.db.forceOpen(project_name, 'image logs').then(function (e,exist) {
         db.log = e
         db.db.forceOpen(project_name, 'image objects').then(function (e) {
             db.object = e
             db.db.forceOpen(project_name, 'image style').then(function (e) {
                 db.style = e
-                db.db.open(app_id, project_name).then(function (e) {
-                    if (e) {
-                        db.project = e
-                        _r()
-                    } else {
-                        db.db.create(app_id, project_name).then(function (e) {
-                            db.project = e
-                            _r()
-                            setTimeout(function () {
-                                var img = new Image();
-                                img.src = "./image/1.png"
-                                img.onload = function () {
-                                    imageToCanvas(img)
-                                }
-                            }, 0)
-                        })
-                    }
-                })
+                _r()
             })
         });
     });
+})
 });
 
+
+// var img = new Image();
+// img.src = "./image/1.png"
+// console.log(db);
+// img.onload = function () {imageToCanvas(img)}
+
+
+// var d=new Promise(function(r){
+// r(7)
+// }).then(function(e){
+// console.log(e);
+
+//     return new Promise(function(r){
+//         setTimeout(function(){
+//             r(3)
+//         },4000)
+//         }).then(function(){
+//             console.log(999);
+//             // console.dir(arguments.callee);
+//         })
+// }).then(function(e){
+// console.log(e);
+// }).then(function(e){
+//     console.log(e);
+
+// });
